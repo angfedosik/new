b3cf6985e4fd6539dfdd099352f2f7136deff52b
angfedosik <angfedosik@gmail.com>
2019-11-26 13:30:01 +0300

    add cleancode

---
+++ cleancode/aircompany/src/by/bsu/testing/aircompany/entity/types/ClassificationLevel.java
@@ -0,0 +1,5 @@
+package by.bsu.testing.aircompany.entity.types;
+
+public enum ClassificationLevel {
+    UNCLASSIFIED, CONFIDENTIAL, SECRET, TOP_SECRET
+}
---
+++ cleancode/aircompany/src/by/bsu/testing/aircompany/entity/types/MilitaryType.java
@@ -0,0 +1,5 @@
+package by.bsu.testing.aircompany.entity.types;
+
+public enum MilitaryType {
+    FIGHTER, BOMBER, TRANSPORT
+}
